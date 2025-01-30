2c38e099239aebc7c21a0257c5e618538e379325
Vassili Tchersky <vt+git@vbc.su>
2025-01-30 21:36:14 +0100

    Add a test case for a tar file containing a sparse file

--- test/test.py
+++ test/test.py
@@ -746,6 +746,10 @@ def TestArchiveWithOptions(options=[]):
             '.': {'ino': 1, 'mode': 'drwxr-xr-x', 'nlink': 2},
             'foo': {'mode': '-rw-r--r--', 'mtime': 1641016352000000000, 'size': 0, 'md5': 'd41d8cd98f00b204e9800998ecf8427e'},
         },
+        'sparse.tar': {
+            '.': {'ino': 1, 'mode': 'drwxr-xr-x', 'nlink': 2},
+            'sparse': {'mode': '-rw-r--r--', 'mtime': 0, 'size': 1073741824, 'md5': '5e4001589ffa2c5135f413a13e6800ef'},
+        }
     }
 
     for zip_name, want_tree in want_trees.items():
