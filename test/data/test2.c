25ca506565dee4cd0d33c1a07d0b8cb8d2a86d9e
François Degros <fdegros@chromium.org>
2025-02-23 11:25:55 +1100

    Add test files

---
+++ test/data/test2.c
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <archive.h>
+
+// gcc -o test2 -Wall test2.c -larchive && ./test2
+int main() {
+  printf(">>> archive_read_new()\n");
+  struct archive* const a = archive_read_new();
+  printf("<<< archive_read_new() returned %p\n\n", a);
+  assert(a != NULL);
+
+  int r;
+  printf(">>> archive_read_append_filter_program()\n");
+  r = archive_read_append_filter_program(a, "base64 -d");
+  printf("<<< archive_read_append_filter_program() returned %d\n\n", r);
+  assert(r == ARCHIVE_OK);
+  
+  printf(">>> archive_read_append_filter_program()\n");
+  r = archive_read_append_filter_program(a, "base64 -d");
+  printf("<<< archive_read_append_filter_program() returned %d\n\n", r);
+  assert(r == ARCHIVE_OK);
+
+  printf(">>> archive_read_support_format_raw()\n");
+  r = archive_read_support_format_raw(a);
+  printf("<<< archive_read_support_format_raw() returned %d\n\n", r);
+  assert(r == ARCHIVE_OK);
+
+  const char* const data = "UTJGdUlIbHZkU0J5WldGa0lIUm9ZWFEvQ2c9PQo=";
+  printf(">>> archive_read_open_memory()\n");
+  r = archive_read_open_memory(a, data, strlen(data));
+  printf("<<< archive_read_open_memory() returned %d\n\n", r);
+  assert(r == ARCHIVE_OK);
+
+  printf(">>> archive_read_free()\n");
+  r = archive_read_free(a);
+  printf("<<< archive_read_free() returned %d\n\n", r);
+  assert(r == ARCHIVE_OK);
+
+  printf("Done\n");
+}
