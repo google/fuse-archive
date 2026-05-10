25566a413c946ed3aca2ba25b5244d403317be5e
Paal Øye-Strømme <paal.o.eye@gmail.com>
2026-05-10 09:35:07 +0200

    fix(test): skip gpg dependent tests on macOS
    Signed-Off-By: Paal Øye-Strømme <paal.o.eye@gmail.com>

--- tests/test.py
+++ tests/test.py
@@ -255,7 +255,10 @@ has_bzip2 = CanRun(['bzip2', '--help'])
 # -h. On Linux, ncompress may or may not be installed and does support -V.
 has_compress = on_mac or CanRun(['compress', '-V'])
 
-has_gpg = CanRun(['gpg', '--version'])
+# On macOS, even if gpg binary is present libarchive can't
+# reach gpg-agent's FD / socket. 
+has_gpg = not on_mac and CanRun(['gpg', '--version'])
+
 has_gzip = CanRun(['gzip', '--version'])
 has_lrzip = CanRun(['lrzip', '--version'])
 has_lz4 = CanRun(['lz4', '--version'])
